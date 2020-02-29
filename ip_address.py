542e721ca372fb6e5735912c6de7e1ab41dfd1f5
Christian <ctf26@alumni.cam.ac.uk>
2020-02-29 11:40:20 +0000

    only git pushing when ip has changed

--- ip_address.py
+++ ip_address.py
@@ -8,8 +8,17 @@ repo = git.Repo()
 while True:
     ip = urllib.request.urlopen("https://api.ipify.org/").read().decode("utf-8")
     with open("ip_address.js", "w") as f:
-        f.write("var ip = \"" + ip + "\"")
-    repo.index.add("ip_address.js")
-    repo.index.commit("automated ip address update")
-    repo.remotes.origin.push()
+        current_line = f.read()
+        
+    new_line = "var ip = \"" + ip + "\""
+    if new_line != current_line:
+        with open("ip_address.js", "w") as f:
+            f.write(new_line)
+        repo.index.add("ip_address.js")
+        repo.index.commit("automated ip address update")
+        repo.remotes.origin.push()
+        print("updated to new ip : ", ip)
+    else:
+        print("same old ip : ", ip)
+
     time.sleep(interval)
