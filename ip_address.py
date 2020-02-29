cd541ef81259031be688ec05b74ac96d95938ff2
Christian <ctf26@alumni.cam.ac.uk>
2020-02-29 11:30:30 +0000

    some python for automated git pushing

--- ip_address.py
+++ ip_address.py
@@ -1,7 +1,13 @@
 import urllib.request, time
+import git
+
+repo = git.Repo()
 
 while True:
     ip = urllib.request.urlopen("https://api.ipify.org/").read().decode("utf-8")
     with open("ip_address.js", "w") as f:
         f.write("var ip = \"" + ip + "\"")
+    repo.index.add("ip_address.js")
+    repo.index.commit("automated ip address update")
+    repo.remotes.origin.push()
     time.sleep(20000)
