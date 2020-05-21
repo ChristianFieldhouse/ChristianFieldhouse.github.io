39fe30a6b84f8e38d95792b50807d8103b96854f
Christian <ctf26@alumni.cam.ac.uk>
2020-05-21 09:18:26 +0100

    now we only care about trump

--- ip_address.py
+++ ip_address.py
@@ -5,12 +5,16 @@ repo = git.Repo()
 
 def wait(minutes=1, seconds=0, notify_seconds=1):
     for i in range((seconds + minutes*60)//notify_seconds):
-        print("                                       ", end="\r")
+        print("                     ", end="\r")
         print(f"fetching in {(seconds + minutes*60)-i*notify_seconds}", end="\r")
         time.sleep(notify_seconds)
 
 while True:
-    ip = urllib.request.urlopen("https://api.ipify.org/").read().decode("utf-8")
+    try:
+        ip = urllib.request.urlopen("https://api.ipify.org/").read().decode("utf-8")
+    except:
+        print("api didn't work, trying again... ", end="\r")
+        wait(minutes=0, seconds=2)
     with open("ip_address.js", "r") as f:
         current_line = f.read()
         
@@ -21,8 +25,8 @@ while True:
         repo.index.add("ip_address.js")
         repo.index.commit("automated ip address update")
         repo.remotes.origin.push()
-        print("updated to new ip : ", ip)
+        print("updated to new ip : ", ip, "                                ")
     else:
-        print("same old ip : ", ip, end="\r")
+        print("                     same old ip : ", ip, end="\r")
 
     wait(minutes=10)
